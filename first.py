95e7aea3bb9ec4307a9c565720cb17fb572d5906
sk_kristina <tinochka2009.18@mail.ru>
2025-01-21 09:56:35 +0300

    ервый коммит

--- first.py
+++ first.py
@@ -1,16 +1,3 @@
 print('Hello, world!')
-print('second commit')
 
-print("Ytllo from FEATURE")
-
-print("Hello from EASYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY")
-
-
-for i in range(9):
-    print(i)
-
-
-print("Первый коммит в feature")
-
-print("Второй коммит")
 
